6a3ce0e5c67d670fdac9ae51d3698c07429c39a9
Oleg Karachun <oleg.karachun.13@gmail.com>
2020-08-23 20:17:17 +0300

    main2

---
+++ Mod07/Labfiles/Starter/Exercise 1/ContosoConf/scripts/ScheduleList.js
@@ -0,0 +1,37 @@
+﻿import { ScheduleItem } from "./ScheduleItem.js";
+
+export class ScheduleList {
+	constructor(element, localStarStorage) {
+		this.element = element;
+		this.localStarStorage = localStarStorage;
+	}
+	async startDownload() {
+		let response = await fetch("/schedule/list");
+		let data = await response.json();
+
+
+		if (response.ok) {
+			this.downloadDone(data);
+		} else {
+			this.downloadFailed();
+		}
+	}
+
+	downloadDone(responseData) {
+		this.addAll(responseData.schedule);
+	}
+
+	downloadFailed() {
+		alert("Could not retrieve schedule data at this time. Please try again later.");
+	}
+
+	addAll(itemsArray) {
+		itemsArray.forEach(this.add, this);
+	}
+
+	add(itemData) {
+		const item = new ScheduleItem(itemData, this.localStarStorage);
+		this.element.appendChild(item.element);
+	}
+}
+
